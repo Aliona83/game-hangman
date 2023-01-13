feac99aef5eaf5e8f3a97f1a366379e5e44950ae
Aliona83 <alionajanusko@gmail.com>
2023-01-13 13:45:15 +0000

    Tru to make colorama working on my text

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-import  colorama
+import colorama
 from colorama import Fore, Style
-  
-def welcome_user():
-   name = input(f"{Fore.RED+Style.BRIGHT}What is your name?\n") 
-   print(name)
+"""
+"""
+colorama.init(autoreset=True)
+name = input(f"{Fore.RED+Style.BRIGHT}What is your name?")
+
