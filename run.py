e91bcd685dbdd546e76a527bf381f2975930091f
Aliona83 <alionajanusko@gmail.com>
2023-01-16 12:51:25 +0000

    Create welcome section, add rules for the game

--- run.py
+++ run.py
@@ -1,7 +1,113 @@
+import random
+from words import WORDS
+import os
 import colorama
+
 from colorama import Fore, Style
+
+from graffiti import welcome
+
 colorama.init(autoreset=True)
+
+def display_hangman(tries):
+   hangman = [
+    '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+========='''
+  ]
+return stage[tries]
+
+
+def hangman_welcome():
+
+
+    
+    """
+ Greeting function with logo  and rules to player to begin the game
+
+   """
+
+
+print(Fore.GREEN + welcome)
+
+def game_start():
+  """
+Create the rules for game and function to start the game
+  """
+
+
+print("Welcome Dear Friend! It is time for game.\n")
+name = input(f"{Fore.RED+Style.BRIGHT}What is your name?\n")
+print("Hello" + " " + name)
+print("Please read the following instructions\n"
+      "to find your way to and trough th game.\n")
+print(
+  "1.You have to guess the secret word one letter at a time\n before you are out of lives.\n"
+)
+print(
+  "2.After each incorrectly answered letter your Hangman will start to  build.\n"
+)
+print("3. You have 6 tries")
+print(
+  "When you reach 0 lives. You will be Hanged!\n Don't worry you can restart the game!"
+)
+print("Good luck ! " + name)
+
+ 
 
 name = None
 name = input(f"{Fore.RED+Style.BRIGHT}What is your name?")
 print(name)
+
+
+def getRandomWords():
+    word = random.choice(WORDS)
+
+  return word.upper()
